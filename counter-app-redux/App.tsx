70651de95cfbb663d864fc608de68052b7cbfb95
Albeiro Crespo <alcresgu_1989@hotmail.com>
2023-04-26 20:34:26 -0500

    install redux/toolkit dependency

--- counter-app-redux/App.tsx
+++ counter-app-redux/App.tsx
@@ -1,18 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
       <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
